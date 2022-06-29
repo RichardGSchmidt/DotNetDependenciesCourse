11314c376fa079f908bfd69397b3f4c20ce6124a
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2022-06-29 11:37:19 -0500

    plodding along

---
+++ IEnumerableTutorial/IEnumerableTutorial/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var array = new int[] {1,2,3 };
+
+            array.GetEnumerator();
+
+            foreach (var a in array) 
+            {
+                Console.WriteLine($"A is {a}");
+            }
+
+            var enumerator = array.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine($"Enumerator A is {enumerator.Current}");
+            }
+
+            var infiniteEnumerable = new MyInfiniteEnumerable();
+
+            foreach (var i in infiniteEnumerable)
+            {
+                Console.WriteLine($"I is{i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MyInfiniteEnumerable : IEnumerable<int>
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return new MyInfiniteEnumerator();
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            return new MyInfiniteEnumerator();
+        }
+    }
+
+    public class MyInfiniteEnumerator : IEnumerator<int>
+    {
+        public int Current { get; private set; } = 0;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+          
+        }
+
+        public bool MoveNext()
+        {
+            Current++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            Current = 0;
+        }
+
+    }
+}
